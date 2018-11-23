959057594f2326e69b6cc3e39ba02028278b3b6a
Nathan Dane <avrovulcanxh607@gmail.com>
2018-11-23 00:33:17 +0000

    BETA Version
    Don't try to load video stories.

--- simplenews.php
+++ simplenews.php
@@ -1,16 +1,17 @@
 <?php
 /*
-	simplenews.php - ALPHA version
+	simplenews.php - BETA version
 	Nathan J. Dane, 2018.
 	Returns a BBC News page as an array
 	
 	Layout (TBC):
-	Short Headline
+	Short Title
 	Description
 	URL
 	Area (e.g. UK)
 	Summary
 	paragraphs(tab seperated)
+	
 */
 
 require "simple_html_dom.php";
@@ -18,10 +19,23 @@ require "simple_html_dom.php";
 function getNews($url,$limit)
 {
 	$html = file_get_html($url);	// Under NO circumstances should $html be overwritten. It's here to stay.
+	if ($html===false) return false;
+	
+	$URL=$html->find("meta[property=og:url]",0);	// URL. The BBC try to hide the AV URL behind a legitamite one, 
+	$URL=$URL->content;								// So we have to take drastic measures to remove them
+	$URL=htmlspecialchars_decode($URL);
+	if(!strncmp($URL,"https://www.bbc.com/news/av/",28)) // Don't even try AV pages
+	{
+		echo "Skipped: AV Story\r\n";
+		return false;
+	}
 	
 	$stitle=$html->find("meta[property=og:title]",0);	// Short title
 	$stitle=$stitle->content;
 	$stitle=htmlspecialchars_decode($stitle);
+	
+	$ltitle=$html->find("title",0)->plaintext;	// Long title
+	$ltitle=htmlspecialchars_decode($ltitle);
 	
 	$desc=$html->find('meta[property=og:description]',0);	// Description
 	$desc=$desc->content;
@@ -41,15 +55,13 @@ function getNews($url,$limit)
 	{
 		if($i<$limit && $found==true)
 		{
-			$paragraph.="$para->plaintext	";
+			$paragraph[]=$para->plaintext;
 			$i++;
 		}
 		if (strpos($para,"introduction"))
 		$found=true;
 	}
-	
-	//echo "$stitle\r\n$desc\r\n$url $area\r\n$intro\r\n$paragraph\r\n";
-	return array($stitle,$desc,$url,$area,$intro,$paragraph);
+	return array($stitle,$ltitle,$desc,$url,$area,$intro,$paragraph);
+	//				1		2		3	4	5		6		7
 }
-
-print_r(getNews("http://www.bbc.co.uk/news/world-europe-44307611",4));
+?>
